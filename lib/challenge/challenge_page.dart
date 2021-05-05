3ce62fc686e5f7ce4f281e7f5eaa2913c1c59089
Diana <dianadsg1@gmail.com>
2021-05-05 18:13:37 -0300

    aulas 1 a 5

---
+++ lib/challenge/challenge_page.dart
@@ -0,0 +1,111 @@
+import 'package:DevQuiz/challenge/challenge_controller.dart';
+import 'package:DevQuiz/result_page/result_page.dart';
+import 'package:flutter/material.dart';
+
+import 'package:DevQuiz/challenge/widgets/next_button/next_button_widget.dart';
+import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
+import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:DevQuiz/models/question_model.dart';
+
+class ChallengePage extends StatefulWidget {
+  final List<QuestionModel> questions;
+  final String title;
+  ChallengePage({
+    Key? key,
+    required this.questions,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  _ChallengePageState createState() => _ChallengePageState();
+}
+
+class _ChallengePageState extends State<ChallengePage> {
+  final controller = ChallengeController();
+  final pageController = PageController();
+  @override
+  void initState() {
+    pageController.addListener(() {
+      controller.currentPage = pageController.page!.toInt() + 1;
+    });
+    super.initState();
+  }
+
+  void nextPage() {
+    if (controller.currentPage < widget.questions.length)
+      pageController.nextPage(
+        duration: Duration(milliseconds: 500),
+        curve: Curves.linear,
+      );
+  }
+
+  void onChange(bool value) {
+    if (value) {
+      controller.rightAnswers++;
+    }
+    nextPage();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(110),
+          child: SafeArea(
+              top: true,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(Icons.close)),
+                  ValueListenableBuilder<int>(
+                    valueListenable: controller.currentPageNotifier,
+                    builder: (context, value, _) => QuestionIndicatorWidget(
+                      currentPage: value,
+                      length: widget.questions.length,
+                    ),
+                  ),
+                ],
+              )),
+        ),
+        body: PageView(
+            physics: NeverScrollableScrollPhysics(),
+            controller: pageController,
+            children: widget.questions
+                .map((e) => QuizWidget(
+                      question: e,
+                      onChange: onChange,
+                    ))
+                .toList()),
+        bottomNavigationBar: SafeArea(
+            bottom: true,
+            child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: ValueListenableBuilder<int>(
+                  valueListenable: controller.currentPageNotifier,
+                  builder: (context, value, _) => Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        if (value == widget.questions.length)
+                          Expanded(
+                              child: NextButtonWidget.green(
+                                  label: "Ver resultados",
+                                  onTap: () {
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => ResultPage(
+                                                  result:
+                                                      controller.rightAnswers,
+                                                  title: widget.title,
+                                                  length:
+                                                      widget.questions.length,
+                                                )));
+                                  }))
+                      ]),
+                ))));
+  }
+}
---
+++ lib/challenge/widgets/next_button/next_button_widget.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:DevQuiz/core/core.dart';
+
+//TODO: trocar a cor do overlay dos botões quando são clicados
+//
+class NextButtonWidget extends StatelessWidget {
+  final String label;
+  final Color backgroundColor;
+  final Color fontColor;
+  final Color borderColor;
+  final VoidCallback onTap;
+
+  const NextButtonWidget({
+    Key? key,
+    required this.label,
+    required this.backgroundColor,
+    required this.fontColor,
+    required this.borderColor,
+    required this.onTap,
+  }) : super(key: key);
+
+  NextButtonWidget.green({required String label, required VoidCallback onTap})
+      : this.backgroundColor = AppColors.darkGreen,
+        this.fontColor = AppColors.white,
+        this.label = label,
+        this.borderColor = AppColors.darkGreen,
+        this.onTap = onTap;
+
+  NextButtonWidget.white({required String label, required VoidCallback onTap})
+      : this.backgroundColor = AppColors.white,
+        this.fontColor = AppColors.grey,
+        this.label = label,
+        this.borderColor = AppColors.border,
+        this.onTap = onTap;
+
+  NextButtonWidget.purple({required String label, required VoidCallback onTap})
+      : this.backgroundColor = AppColors.purple,
+        this.fontColor = AppColors.white,
+        this.label = label,
+        this.borderColor = AppColors.border,
+        this.onTap = onTap;
+
+  NextButtonWidget.transparent(
+      {required String label, required VoidCallback onTap})
+      : this.backgroundColor = Colors.transparent,
+        this.fontColor = AppColors.grey,
+        this.label = label,
+        this.borderColor = Colors.transparent,
+        this.onTap = onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 48,
+        child: TextButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(backgroundColor),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10))),
+                side:
+                    MaterialStateProperty.all(BorderSide(color: borderColor))),
+            onPressed: onTap,
+            child: Text(label,
+                style: GoogleFonts.notoSans(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15,
+                    color: fontColor))));
+  }
+}
